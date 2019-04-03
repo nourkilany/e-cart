e4e3225313a7b3b81cb79434144425a315874cae
nour <nour.kilany88@gmail.com>
2019-04-03 21:46:24 +0200

    Add footer template

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  selector: "app-footer",
+  template: `
+  <footer class="page-footer font-small bg-primary">  
+    <div id="footer-text" class="footer-copyright text-center py-3">© {{year}} Copyright: Nour Kilany
+    </div>
+  </footer>`,
+  styleUrls: ["./footer.component.scss"]
 })
 export class FooterComponent implements OnInit {
-  year:number = new Date().getFullYear();
-  constructor() { }
+  year: number = new Date().getFullYear();
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
