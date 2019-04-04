c8a0a4a8c59c81776d40fe181a7a062342ca0054
nour <nour.kilany88@gmail.com>
2019-04-04 10:10:41 +0200

    Update header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    WishlistComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
