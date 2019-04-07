c25f08eb702c7583ca247575fc940607e2fdbe5b
nour <nour.kilany88@gmail.com>
2019-04-07 15:50:15 +0200

    Add products component

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+  products: Product[];
+  constructor(private productsService:ProductsService) { }
+
+  ngOnInit() {
+    this.products = this.productsService.getProducts();
+  }
+
+  wishProduct(product:Product){
+    console.table(product);
+  }
+}
