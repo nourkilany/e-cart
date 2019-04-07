38daa7c1c0b70373990ff04d459da32c3da14dc4
nour <nour.kilany88@gmail.com>
2019-04-07 15:52:53 +0200

    Add view single product page component

---
+++ src/app/components/view-page/view-page.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProductsService } from 'src/app/services/products.service';
+import { Product } from 'src/app/models/product.model';
+
+@Component({
+  selector: 'app-view-page',
+  templateUrl: './view-page.component.html',
+  styleUrls: ['./view-page.component.scss']
+})
+export class ViewPageComponent implements OnInit {
+  public product_id: any;
+  products: Product[];
+  constructor(private router: Router, private route: ActivatedRoute, private productsService:ProductsService) { }
+
+  ngOnInit() {
+    this.products = this.productsService.getProducts();
+    this.product_id = this.route.params['value'].id;
+    // console.table()
+  }
+
+}
