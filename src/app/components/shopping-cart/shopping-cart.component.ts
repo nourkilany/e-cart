e0bfef03cd29e48589e32eef417bf7669b6f3eac
nour <nour.kilany88@gmail.com>
2019-04-10 00:15:36 +0200

    Modify shopping cart

--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-shopping-cart',
-  template: `
-    <div>
-      <a class="nav-link" href="javascript:;;">
-        <i class="fa fa-shopping-cart">
-          <span class="mt-1"> 0</span>
-        </i>
-      </a>
-    </div>
-  `,
+  templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.scss']
 })
 export class ShoppingCartComponent implements OnInit {
-  constructor() {}
+  cartCounter: number = 0;
+  cart_products: Product[] = [];
+  constructor(private cart: CartService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.initialize();
+  }
+
+  deleteProduct(id) {
+    this.cart.deleteItem(id);
+    this.initialize();
+  }
+
+  initialize(){
+    this.cart.observable.subscribe(() => this.cartCounter = this.cart.getCartCount());
+    this.cart_products = this.cart.getCart();
+  }
 }
