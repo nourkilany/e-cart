183e1eaa378a5c9dbb27e32b0f2bf19c464a298b
nour <nour.kilany88@gmail.com>
2019-04-09 18:12:57 +0200

    Add view page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
+import { ViewPageComponent } from './components/view-page/view-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: ProductsComponent },
+  { path: 'products/:id',component: ViewPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
