bfc84d419c2f279a6c4262da88f02a6c305c7e54
nour <nour.kilany88@gmail.com>
2019-04-10 16:00:09 +0200

    Update layout and add related products

--- src/app/components/view-page/view-page.component.ts
+++ src/app/components/view-page/view-page.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ProductsService } from 'src/app/services/products.service';
 import { Product } from 'src/app/models/product.model';
+import { WishlistService } from 'src/app/services/wishlist.service';
+import { CartService } from 'src/app/services/cart.service';
+
 
 @Component({
   selector: 'app-view-page',
@@ -9,13 +12,44 @@ import { Product } from 'src/app/models/product.model';
   styleUrls: ['./view-page.component.scss']
 })
 export class ViewPageComponent implements OnInit {
+  public product;
   public product_id: any;
-  products: Product[];
-  constructor(private router: Router, private route: ActivatedRoute, private productsService:ProductsService) { }
+  public similar_products: Product[] = [];
+  
+  
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private productsService: ProductsService,
+    private wishlistService: WishlistService,
+    private cartService: CartService
+    ) { }
 
   ngOnInit() {
-    this.products = this.productsService.getProducts();
+    let products = this.productsService.getProducts();
     this.product_id = this.route.params['value'].id;
+    products.forEach((product => {
+      
+      if (product.id === this.product_id) {
+        this.product = product;
+      }
+    }))
+    products.forEach((product)=>{
+      if (product.category === this.product.category && product.id !== this.product_id) {
+        this.similar_products.push(product);
+      }
+    })
+
+  }
+  onWish() {
+    this.wishlistService.addToWishlist(this.product);
+  }
+  onCart() {
+    this.cartService.addToCart(this.product);
+  }
+  onSwitch(product){
+    this.product = product;
   }
 
 }
