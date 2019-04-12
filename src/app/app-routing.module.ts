62f2528ffc36d8bae165191a190af8b0e08abb97
nour <nour.kilany88@gmail.com>
2019-04-12 10:39:15 +0200

    Add not found component and page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { ProductsComponent } from './components/products/products.component';
 import { ViewPageComponent } from './components/view-page/view-page.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { RegisterComponent } from './components/register/register.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   { path:'', component: ProductsComponent },
   { path: 'products/:id',component: ViewPageComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: 'register', component: RegisterComponent },
-
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '404'}
 ];
 
 @NgModule({
